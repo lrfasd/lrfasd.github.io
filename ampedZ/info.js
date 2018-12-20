f9d09641af0bfcc6603042f41fa3391459283e58
lrfasd <lrfasd@163.com>
2018-12-20 18:22:35 +0800

    Update info.js

--- ampedZ/info.js
+++ ampedZ/info.js
@@ -1,1 +1,1 @@
-(function(){window.Info={"file":["ampedZ.zar","ampedZ_r.zar"],"length":["643.35 KB","1.79 MB"],"intro":["设定","设定、资源"],"operate":["",""]};})();
+(function(){window.Info={"file":["ampedZ.zar","ampedZ_r.zar"],"length":["642.81 KB","2.20 MB"],"intro":["设定","设定、资源"],"operate":["",""]};})();
