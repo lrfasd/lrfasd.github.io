713f66293500b87f2cebc00b577ae2d43ae47898
lrfasd <lrfasd@163.com>
2019-05-09 11:08:43 +0800

    Update info.js

--- ampedZ/info.js
+++ ampedZ/info.js
@@ -1,1 +1,1 @@
-(function(){window.Info={"file":["ampedZ.zar","ampedZ_r.zar"],"length":["655.33 KB","5.97MB"],"intro":["设定","设定、资源"],"operate":["",""]};})();
+(function(){window.Info={"file":["ampedZ.zar","ampedZ_r.zar"],"length":["655.33 KB","6.02MB"],"intro":["设定","设定、资源"],"operate":["",""]};})();
