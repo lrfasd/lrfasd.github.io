6ad4101f27472a6ce792538d61b098620018e338
lrfasd <lrfasd@163.com>
2019-12-24 10:29:31 +0800

    Add files via upload

--- ampedZ/info.js
+++ ampedZ/info.js
@@ -1,1 +1,1 @@
-(function(){window.Info={"file":["ampedZ.zar","ampedZ_d.zar","ampedZ_r.zar","ampedZ_dr.zar"],"length":["682.42KB","984.83KB","6.20MB","6.50MB"],"intro":["设定","设定、字典","设定、资源","设定、字典、资源"],"operate":["","","",""]};})();
+(function(){window.Info={"file":["ampedZ.zar","ampedZ_d.zar","ampedZ_r.zar","ampedZ_dr.zar"],"length":["685.44KB","985.64KB","6.21MB","6.50MB"],"intro":["设定","设定、字典","设定、资源","设定、字典、资源"],"operate":["","","",""]};})();
