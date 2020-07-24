966df17d9868041a5431223d59b0cc9d64eb3eec
lrfasd <lrfasd@163.com>
2020-07-24 12:00:31 +0800

    Add files via upload

--- ampedZ/info.js
+++ ampedZ/info.js
@@ -1,1 +1,1 @@
-(function(){window.Info={"file":["ampedZ_d.zar","ampedZ_dr.zar"],"length":["1.04MB","6.74MB"],"intro":["数据","数据、资源"],"operate":["","https://www.jianguoyun.com/p/DUpiWhQQitn5Bxj1ma4D"]};window.Intro="数据更新：2020.07.19 08:37:21"})();
+(function(){window.Info={"file":["ampedZ_d.zar","ampedZ_dr.zar"],"length":["1.023.68KB","6.72MB"],"intro":["数据","数据、资源"],"operate":["","https://www.jianguoyun.com/p/DUpiWhQQitn5Bxj1ma4D"]};window.Intro="数据更新：2020.07.24 11:58:33"})();
