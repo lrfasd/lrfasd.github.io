f08aab563bf0ebee5bb1ca91798953b57c5da28d
lrfasd <lrfasd@163.com>
2021-09-11 12:43:40 +0800

    Delete info.js

--- ampedZ/info.js
+++
@@ -1,1 +0,0 @@
-(function(){window.Info={"file":["ampedZ_d.zar","ampedZ_dr.zar"],"length":["1.06MB","6.81MB"],"intro":["数据","数据、资源"],"operate":["","https://www.jianguoyun.com/p/DUpiWhQQitn5Bxj1ma4D"]};window.Intro="数据更新：2020.08.08 12:05:44"})();
